9b127d397d9cf6d312162e2c311ef91ace48a9d7
Jose Lopes <jabolopes@google.com>
2019-12-03 09:28:31 +0100

    Add benchmark.

---
+++ benchmark.js
@@ -0,0 +1,173 @@
+async function measure(fn) {
+  var start = performance.now();
+  await fn();
+  var end = performance.now();
+  console.log('Run: ', end - start);
+}
+
+var benchWorker = 'web';
+
+function init() {
+  switch (benchWorker) {
+    case 'web':
+      websql_init();
+      break;
+    case 'shared':
+      websql_init_shared();
+      break;
+    default:
+      throw 'Unknown worker';
+  }
+}
+
+async function enable_mmap(connection) {
+  const mmap_size = 10485760;
+  await websql_exec(connection, `PRAGMA mmap_size=${mmap_size};`, row => { });
+}
+
+async function disable_mmap(connection) {
+  await websql_exec(connection, "PRAGMA mmap_size=0;", row => { });
+}
+
+async function single(file) {
+  init();
+
+  connection = await websql_open(file);
+  try {
+    await websql_exec(connection, "CREATE TABLE tbl(name varchar(100));", row => { });
+  } catch (e) {}
+
+  await websql_exec(connection, "INSERT INTO tbl VALUES ('hello');", row => { });
+  await websql_exec(connection, "SELECT * FROM tbl;", row => { });
+  await websql_close(connection);
+}
+
+var runs = 1000;
+
+async function many_rw(file, use_mmap) {
+  var n = runs;
+
+  try {
+    await fsUnlink(file);
+  } catch (e) {
+    if (e.code !== "ENOENT") {
+      throw e;
+    }
+  }
+
+  connection = await websql_open(file);
+  if (use_mmap) {
+    await enable_mmap(connection);
+    await websql_exec(connection, `PRAGMA mmap_size;`, row => { console.log(row); });
+  }
+
+  try {
+    await websql_exec(connection, "CREATE TABLE tbl(name varchar(100));", row => { });
+  } catch (e) {}
+
+  var rows = 0;
+  for (var i = 0; i < n; ++i) {
+    await websql_exec(connection, `INSERT INTO tbl VALUES ('${i}');`, row => { });
+    await websql_exec(connection, `SELECT * FROM tbl WHERE name = '${i}';`, row => { ++rows });
+  }
+
+  if (rows != n) {
+    console.log(`${file}: readwrite: rows = ${rows}; want ${n} rows`);
+  }
+
+  await websql_close(connection);
+}
+
+async function many_wo(file, use_mmap) {
+  var n = runs;
+
+  try {
+    await fsUnlink(file);
+  } catch (e) {
+    if (e.code !== "ENOENT") {
+      throw e;
+    }
+  }
+
+  connection = await websql_open(file);
+  if (use_mmap) {
+    await enable_mmap(connection);
+    await websql_exec(connection, `PRAGMA mmap_size;`, row => { console.log(row); });
+  }
+
+  try {
+    await websql_exec(connection, "CREATE TABLE tbl(name varchar(100));", row => { });
+  } catch (e) {}
+
+  for (var i = 0; i < n; ++i) {
+    await websql_exec(connection, "INSERT INTO tbl VALUES ('" + i + "');", row => { })
+  }
+
+  await websql_close(connection);
+}
+
+async function many_ro(file, use_mmap) {
+  var n = runs;
+
+  connection = await websql_open(file);
+  if (use_mmap) {
+    await enable_mmap(connection);
+  }
+
+  var rows = 0;
+  for (var i = 0; i < n; ++i) {
+    await websql_exec(connection, `SELECT * FROM tbl WHERE name = '${i}';`, row => { ++rows });
+  }
+
+  if (rows != n) {
+    console.log(`${file}: readonly: rows = ${rows}; want ${n} rows`);
+  }
+
+  await websql_close(connection);
+}
+
+async function run_iofs() {
+  console.log('Running IOFS benchmark');
+
+  init();
+
+  await measure(async () => {
+    await many_wo('/io/hello.db');
+    await many_ro('/io/hello.db');
+    await many_rw('/io/hello.db');
+  });
+
+  websql_profile();
+}
+
+async function run_iofs() {
+  console.log('Running IOFS benchmark');
+
+  init();
+
+  await measure(async () => {
+    await many_wo('/io/hello.db');
+    await many_ro('/io/hello.db');
+    await many_rw('/io/hello.db');
+  });
+
+  websql_profile();
+}
+
+async function run_chromefs() {
+  console.log('Running Chrome FS benchmark');
+
+  init();
+
+  await measure(async () => {
+    await many_wo('/chrome/hello.db');
+    await many_ro('/chrome/hello.db');
+    await many_rw('/chrome/hello.db');
+  });
+
+  websql_profile();
+}
+
+// Run the benchmark(s):
+// await run_iofs()
+// await run_chromefs()
